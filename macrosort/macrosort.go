a5ec39912bdb04e05ae322fb8debc0d4e3beca22
anlhord <anlhord@gmail.com>
2015-01-04 13:35:56 +0100

    Improve the reference macrosort solution

--- macrosort/macrosort.go
+++ macrosort/macrosort.go
@@ -1,14 +1,28 @@
+/*
+generic n^2 sort
+Copyright (C) 2015  github.com/anlhord
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 package main
 
 import "unsafe"
 import "reflect"
 import "math"
 import "fmt"
-
-// memsizetyper provides sizes and (optionally) types to macros
-type memsizetyper struct {
-	siz int
-}
 
 // Type specific compar functions
 func Compar32(a, b *float32) int {
@@ -19,18 +33,26 @@ func Compar32(a, b *float32) int {
 
 // Type specific compar functions
 func Compar64(a, b *float64) int {
-	oa := int(math.Float64bits(*a))
-	ob := int(math.Float64bits(*b))
-	return int(oa - ob)
+	oa := int64(math.Float64bits(*a))
+	ob := int64(math.Float64bits(*b))
+	ab := oa - ob
+	m := int(int64(ab >> 32))
+	if m == 0 {
+		return int(ab)
+	}
+	return m
 }
 
-type Comparer interface {
-	Comparer(m *memsizetyper) func(a, b unsafe.Pointer) int
+// Comparerer macro-interface
+type Comparerer interface {
+	Comparer(m *[]int) func(a, b unsafe.Pointer) int
 }
+
+type Comparert struct{}
 
 // Comparer macro
-func Comparer(m *memsizetyper) func(a, b unsafe.Pointer) int {
-	switch m.siz {
+func (Comparert) Comparer(ts0 *[]int) func(a, b unsafe.Pointer) int {
+	switch (*ts0)[0] {
 	case 4:
 		return func(a, b unsafe.Pointer) int { return Compar32((*float32)(a), (*float32)(b)) }
 	case 8:
@@ -41,15 +63,18 @@ func Comparer(m *memsizetyper) func(a, b unsafe.Pointer) int {
 }
 
 // Sort macro
-func Sort(s reflect.SliceHeader, comparer func(m *memsizetyper) func(a, b unsafe.Pointer) int, m *memsizetyper) {
-	compar := comparer(m)
+func Sort(ts0 *[]int, s reflect.SliceHeader, comparer Comparerer) {
+
+//	fmt.Println("HELLOU", s.Len)
+	compar := comparer.Comparer(ts0)
 	for i := 0; i < s.Len; i++ {
 		for j := 0; j < i; j++ {
-			slicei := unsafe.Pointer(s.Data + uintptr(i*m.siz))
-			slicej := unsafe.Pointer(s.Data + uintptr(j*m.siz))
+			slicei := unsafe.Pointer(s.Data + uintptr(i*(*ts0)[0]))
+			slicej := unsafe.Pointer(s.Data + uintptr(j*(*ts0)[0]))
 			if compar(slicei, slicej) > 0 {
-				fmt.Println((*[]float32)(unsafe.Pointer(&s)))
-				for q := 0; q < m.siz; q++ {
+//				fmt.Println("hi")
+//				fmt.Println((*[]float64)(unsafe.Pointer(&s)))
+				for q := 0; q < (*ts0)[0]; q++ {
 					sli := (*byte)(unsafe.Pointer(uintptr(slicei) + uintptr(q)))
 					slj := (*byte)(unsafe.Pointer(uintptr(slicej) + uintptr(q)))
 					/*
@@ -61,7 +86,9 @@ func Sort(s reflect.SliceHeader, comparer func(m *memsizetyper) func(a, b unsafe
 					*sli = *slj
 					*slj = x
 				}
-				fmt.Println((*[]float32)(unsafe.Pointer(&s)))
+//				fmt.Println((*[]float64)(unsafe.Pointer(&s)))
+			} else {
+//				fmt.Println("ho")
 			}
 
 		}
@@ -71,14 +98,23 @@ func Sort(s reflect.SliceHeader, comparer func(m *memsizetyper) func(a, b unsafe
 
 func main() {
 	var sa = []float32{818, 128, 39, 153, 643}
-	var sb = []float32{9, 8, 7, 6, 5, 4, 3, 1, 2}
+	var sb = []float64{456, 7894, 354, 134, 978, 354, 2254, 85, 4567}
 	_ = sa
 	_ = sb
 
-	var m memsizetyper
-	m.siz = int(unsafe.Sizeof(sa[0]))
+	// construct the type stacks
 
-	Sort(*(*reflect.SliceHeader)((unsafe.Pointer(&sa))), Comparer, &m)
+	var m, n []int
+	m = append(m, int(unsafe.Sizeof(sa[0])))
+	n = append(n, int(unsafe.Sizeof(sb[0])))
+
+	// run the sorts
+
+	Sort(&m, *(*reflect.SliceHeader)((unsafe.Pointer(&sa))), Comparert{})
+	Sort(&n, *(*reflect.SliceHeader)((unsafe.Pointer(&sb))), Comparert{})
+
+	// print the results
 
 	fmt.Println(sa)
+	fmt.Println(sb)
 }
